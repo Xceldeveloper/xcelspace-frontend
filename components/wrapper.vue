e715d07f4eebb499c168c3a3205bc06874b32092
Xceldeveloper <56355878+Xceldeveloper@users.noreply.github.com>
2021-06-01 17:13:30 +0100

    custom wrapper component

---
+++ components/wrapper.vue
@@ -0,0 +1,24 @@
+<template>
+  <div  :style="`height:${height};width:${width}`">
+      <slot>
+      </slot>
+  </div>
+</template>
+
+<script>
+export default {
+    props:{
+        height:{
+            default:"auto"
+        },
+        width:{
+            default:"auto"
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+
+</style>
